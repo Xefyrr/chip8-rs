394ba23bde8081257effa97036ae90731331e80d
Will Banyard <will.banyard@outlook.com>
2023-07-06 02:03:26 +0100

    Added window creation for Chip-8 display

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+extern crate sdl2;
+
+use sdl2::event::Event;
+
+const WINDOW_SCALE: u32 = 20;
+const SIZE_WIDTH: u32 = 64;
+const SIZE_HEIGHT: u32 = 32;
+
+fn main() -> Result<(), String> {
+    let sdl_ctx = sdl2::init()?;
+    let sdl_video = sdl_ctx.video()?;
+
+    let _window = sdl_video.window("CHIP-8 Emulator", SIZE_WIDTH * WINDOW_SCALE, SIZE_HEIGHT * WINDOW_SCALE).position_centered().build().expect("Unable to initialise window!");
+    let mut event_pump = sdl_ctx.event_pump()?;
+
+    'running: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit {..} => {
+                    break 'running;
+                },
+                _ => {}
+            }
+        }
+    }
+
+    Ok(())
 }
