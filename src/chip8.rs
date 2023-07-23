daeedb7658e9e276991310c372a7d67321f85c17
Will Banyard <will.banyard@outlook.com>
2023-07-23 01:58:46 +0100

    Implemented instructions and rom loading
    Chip8 instructions for testing IBM Logo rom have been implemented, along
    with functionality for loading roms into memory

--- src/chip8.rs
+++ src/chip8.rs
@@ -1,7 +1,7 @@
-use SCREEN_WIDTH;
-use SCREEN_HEIGHT;
+use crate::SCREEN_WIDTH;
+use crate::SCREEN_HEIGHT;
 
-struct Chip8 {
+pub struct Chip8 {
     memory: [u8; 4096],
     video: [[bool; SCREEN_WIDTH]; SCREEN_HEIGHT],
     registers: [u8; 16],
@@ -14,10 +14,10 @@ struct Chip8 {
 }
 
 impl Chip8 {
-    fn init() -> self {
+    pub fn init() -> Self {
         Chip8 {
             memory: [0; 4096],
-            video: [[0; SCREEN_WIDTH]; SCREEN_HEIGHT],
+            video: [[false; SCREEN_WIDTH]; SCREEN_HEIGHT],
             registers: [0; 16],
             i_reg: 0,
             delay_timer: 0,
@@ -25,6 +25,97 @@ impl Chip8 {
             pc: 0x200,
             sp: 0,
             stack: [0; 16],
+        }
+    }
+
+    pub fn load_rom(&mut self, buf: &Vec<u8>) {
+        let start: usize = 0x200; // May be better as a const value for starting address.
+        let end: usize = start + buf.len();
+        self.memory[start..end].copy_from_slice(buf);
+    }
+
+    pub fn tick(&mut self) {
+        let opcode = self.fetch();
+        self.execute(opcode);
+    }
+
+    fn fetch(&mut self) -> u16 {
+        let high = self.memory[self.pc as usize] as u16;
+        let low = self.memory[(self.pc + 1) as usize] as u16;
+        let op = (high << 8) | low;
+
+        self.pc += 2;
+        op
+    }
+
+    fn execute(&mut self, op: u16) {
+        let nibbles = (
+            (op & 0xF000) >> 12,
+            (op & 0x0F00) >> 8,
+            (op & 0x00F0) >> 4,
+            (op & 0x000F),
+        );
+
+        let nnn = (op & 0x0FFF);
+        let nn = (op & 0x00FF) as u8;
+        let x = nibbles.1 as usize;
+        let y = nibbles.2 as usize;
+        let n = nibbles.3;
+
+        match nibbles {
+            // CLS
+            (0x0, 0x0, 0xE, 0x0) => {
+                self.video = [[false; SCREEN_WIDTH]; SCREEN_HEIGHT];
+            },
+
+            // JP nnn
+            (0x1, _, _, _) => {
+                self.pc = nnn;
+            },
+
+            // LD Vx, nn
+            (0x6, _, _, _) => {
+                self.registers[x] = nn;
+            },
+
+            // ADD
+            (0x7, _, _, _) => {
+                self.registers[x] += nn;
+            },
+
+            // LD I, addr
+            (0xA, _, _, _) => {
+                self.i_reg = nnn;
+            },
+            
+            // DRW Vx, Vy, n
+            (0xD, _, _, _) => {
+                let mut has_flipped = false;
+
+                for rows in 0..n {
+                    let addr = self.i_reg + rows as u16;
+                    let pixels = self.memory[addr as usize];
+
+                    for cols in 0..8 {
+                        if (pixels & (0b1000_0000 >> cols)) == 1 {
+                            let x_coord = (self.registers[x] as u16 + cols) as usize % SCREEN_WIDTH;
+                            let y_coord = (self.registers[y] as u16 + rows) as usize % SCREEN_HEIGHT;
+
+                            has_flipped |= self.video[x_coord][y_coord];
+                            self.video[x_coord][y_coord] ^= true;
+                        }
+                    }
+                }
+
+                if has_flipped {
+                    self.registers[0xF] = 1;
+                }
+                else {
+                    self.registers[0xF] = 0;
+                }
+            }
+
+            (_, _, _, _) => unimplemented!("Opcode not Implemented! (Opcode: {})", op),
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,41 @@
 extern crate sdl2;
 
+mod chip8;
+use chip8::Chip8;
+
+use sdl2::render::Canvas;
+use sdl2::video::Window;
+
+use std::fs::File;
+use std::io::Read;
+use std::env;
+
 use sdl2::event::Event;
 
+const SCREEN_WIDTH: usize = 64;
+const SCREEN_HEIGHT: usize = 32;
+
 const WINDOW_SCALE: u32 = 20;
-const SCREEN_WIDTH: u32 = 64;
-const SCREEN_HEIGHT: u32 = 32;
+const WINDOW_WIDTH: u32 = (SCREEN_WIDTH as u32) * WINDOW_SCALE;
+const WINDOW_HEIGHT: u32 = (SCREEN_HEIGHT as u32) * WINDOW_SCALE;
 
 fn main() {
+    let args: Vec<_> = env::args().collect();
+
     let sdl_context = sdl2::init().unwrap();
     let video_subsys = sdl_context.video().unwrap();
+    let _window = video_subsys.window("CHIP-8 Emulator", WINDOW_WIDTH, WINDOW_HEIGHT).position_centered().opengl().build().expect("Unable to initialise window!");
 
-    let _window = video_subsys.window("CHIP-8 Emulator", SCREEN_WIDTH * WINDOW_SCALE, SCREEN_HEIGHT * WINDOW_SCALE).position_centered().opengl().build().expect("Unable to initialise window!");
+    let mut canvas = _window.into_canvas().present_vsync().build().unwrap();
     let mut event_pump = sdl_context.event_pump().unwrap();
+
+    let mut chip8 = Chip8::init();
+
+    let mut rom = File::open(&args[1]).expect("Unable to open file!");
+    let mut buffer = Vec::new();
+
+    rom.read_to_end(&mut buffer).unwrap();
+    chip8.load_rom(&buffer);
 
     'running: loop {
         for event in event_pump.poll_iter() {
@@ -22,5 +46,14 @@ fn main() {
                 _ => {}
             }
         }
+        
+        chip8.tick();
+        draw(&chip8, &mut canvas);
     }
+}
+
+fn draw(chip8: &Chip8, canvas: &mut Canvas<Window>) {
+    // 1. Clear canvas
+    // 2. Loop through screen
+    // 3. Create scaled rectangles where pixels on the chip8 are true
 }
